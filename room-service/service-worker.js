887c4a442d35b2ed4c2bee06fdcf1cdef6b10d41
adamgrabczynski92-alt <adam.grabczynski92@gmail.com>
2026-06-11 23:00:47 +0200

    aplikacja

--- room-service/service-worker.js
+++ room-service/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'adago-room-service-v11';
+const CACHE = 'adago-room-service-v22-oaza-bez-mlynka';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
